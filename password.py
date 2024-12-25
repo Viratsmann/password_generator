6526e8fa053a5b2eecc85aad5075210773edaa06
viratmann <viratsmann@gmail.com>
2024-12-25 10:44:18 -0800

    Initial commit

---
+++ password.py
@@ -0,0 +1,22 @@
+import random
+import string
+
+def generate_password(length):
+    characters = string.ascii_letters + string.digits + string.punctuation
+    password = "".join(random.choice(characters) for i in range(length))
+    return password
+
+def main():
+    print("Welcome to Password Generator")
+    try:
+        length = int(input("How long would you like your password to be? "))
+        if length < 1:
+            print("Please enter a positive integer")
+        else:
+            password = generate_password(length)
+            print(f"Your Password is: {password}")
+    except ValueError:
+            print("Please enter a positive integer")
+
+if __name__ == "__main__":
+    main()
